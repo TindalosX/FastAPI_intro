78d68a2b73d6f982f24b67a7e8a8b018c2a51917
5thDemensionOfBugs <5thDemensionOfBugs@ejemplo.com>
2026-05-15 01:49:20 -0600

    Add dummy data and an endpoint function for that dummy data

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+posts: list[dict] = [
+    {"id": 1, "author": "Author 1", "title": "Post 1", "content": "Content 1"},
+    {"id": 2, "author": "Author 2", "title": "Post 2", "content": "Content 2"},
+    {"id": 3, "author": "Author 3", "title": "Post 3", "content": "Content 3"},
+]
+
 @app.get("/")
 def home():
     return {"Hello": "World"}
+
+@app.get("/api/posts")
+def get_posts():
+    return posts
